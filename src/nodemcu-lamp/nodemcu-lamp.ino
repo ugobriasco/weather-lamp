92c4c31bf86321f8a30a9e130666fd81adfcde9e
Ugo <ugo.briasco@sumup.com>
2020-10-23 10:36:21 +0200

    cleanup

--- src/nodemcu-lamp/nodemcu-lamp.ino
+++ src/nodemcu-lamp/nodemcu-lamp.ino
@@ -3,7 +3,6 @@
 #include <ESP8266WebServer.h>
 #include <ESP8266HTTPClient.h>
 #include <ArduinoJson.h>
-
 #include "config.h"
 
 //EXP8266
@@ -17,28 +16,44 @@ int LEDPin = 5;
 // Weather API relation
 const size_t capacity = 49*JSON_ARRAY_SIZE(1) + JSON_ARRAY_SIZE(48) + 11*JSON_OBJECT_SIZE(1) + 49*JSON_OBJECT_SIZE(4) + JSON_OBJECT_SIZE(6) + 37*JSON_OBJECT_SIZE(12) + 11*JSON_OBJECT_SIZE(13) + JSON_OBJECT_SIZE(14) + 1600;
 DynamicJsonDocument doc(capacity);
-int HOURLY_FORECAST = 4;
-//const char *host = "http://arduinojson.org/example.json";
+float temp = 0;
+int weatherID = 0;
 
-// Cronjob
-int cron_1 = 60;  //60 Seconds intervall for timer 1;
-long t1;
 
 // Cronjobs
+int cron_1 = 300;  //300 Seconds (5 min) intervall for timer 1;
+int cron_2 = 600;  //600 Seconds (10 min) intervall for timer 1;
+    long t1;
+    long t2;
 void cron1() {
-  handleHttpRequest();
+  getWeatherData();
 }
-
-void cronjob() {
+void cron2() {
+  printUsedWiFi();
+}
+void cronjobs() {
   long tmp = millis();
   if ((t1 + (cron_1 * 1000)) <= tmp) {
     cron1();
     t1 = millis();
   }
+  if ((t2 + (cron_2 * 1000)) <= tmp) {
+    cron2();
+    t2 = millis();
+  }
 }
 
-// Get weather from Openweather API
-void handleHttpRequest(void){
+// Logging
+void printUsedWiFi(void){
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(SSID);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+// Get weather from Openweather API and parse data in `temp` and `weatherID`.
+void getWeatherData(void){
   Serial.println("Cronjob 1 active");
   HTTPClient http;
   Serial.println("Getting weather ..");
@@ -50,13 +65,8 @@ void handleHttpRequest(void){
   Serial.print("Response Code:"); //200 is OK
   Serial.println(httpCode);   //Print HTTP return code
 
-  // Serial.print("Returned data from Server:");
-  // Serial.println(payload);    //Print request response payload
-
   if(httpCode == 200)
   {
-    //JsonObject root = jsonBuffer.parseObject(payload);
-
     deserializeJson(doc, payload);
     DeserializationError error = deserializeJson(doc, payload);
     if (error){
@@ -64,17 +74,19 @@ void handleHttpRequest(void){
     } else {
 
       JsonArray hourly = doc["hourly"];
-      float temp = hourly[HOURLY_FORECAST]["temp"];
-      int weatherID = hourly[HOURLY_FORECAST]["weather"][0]["id"]; // 800
-      const char* weatherLabel = hourly[HOURLY_FORECAST]["weather"][0]["main"]; // "Clear
-      Serial.println("Weather forecast in 4 hours:");
-      Serial.print("Temperature: ");
-      Serial.println(temp);
-      Serial.print("Weather: ");
+      temp = hourly[FORECAST_IN_HOURS]["feels_like"];
+      weatherID = hourly[FORECAST_IN_HOURS]["weather"][0]["id"]; // 800
+      const char* weatherLabel = hourly[FORECAST_IN_HOURS]["weather"][0]["main"]; // "Clear
+      Serial.print("Weather forecast in ");
+      Serial.print(FORECAST_IN_HOURS);
+      Serial.println(" hours:");
+      Serial.print("Temperature (feels-like):\t");
+      Serial.print(temp);
+      Serial.println("C");
+      Serial.print("Weather:\t\t");
       Serial.print(weatherID);
       Serial.print("-");
       Serial.println(weatherLabel);
-
     };
   }
   else
@@ -84,31 +96,10 @@ void handleHttpRequest(void){
   http.end();
 }
 
-void setup(void){
+// Run webserver to handle the lamp manually.
+void startWebServer(void){
 
-  pinMode(LEDPin, OUTPUT);
-  digitalWrite(LEDPin, LOW);
-
-  delay(1000);
-  Serial.begin(115200);
-  WiFi.begin(SSID, PASSWORD);
-  Serial.println("");
-
-  // Wait for connection
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println(SSID);
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
-
-
-  // Run webserver
   page = "<h1>THE WEATHERLAMP!</h1><p><a href=\"LEDOn\"><button>ON</button></a>&nbsp;<a href=\"LEDOff\"><button>OFF</button></a></p>";
-
   server.on("/", [](){
     server.send(200, "text/html", page);
   });
@@ -126,7 +117,28 @@ void setup(void){
   Serial.println("Web server started!");
 }
 
+//main
+void setup(void){
+
+  pinMode(LEDPin, OUTPUT);
+  digitalWrite(LEDPin, LOW);
+
+  delay(1000);
+  Serial.begin(115200);
+  WiFi.begin(SSID, PASSWORD);
+  Serial.println("");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  printUsedWiFi();
+  startWebServer();
+  getWeatherData();
+}
+
 void loop(void){
   server.handleClient();
-  cronjob();
+  cronjobs();
 }
